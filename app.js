e295c4334bd3dc93d41def5ee9bc0de710624ffd
AhmedHazzazi <40734018+AhmedHazzazi@users.noreply.github.com>
2022-01-06 14:15:21 +0300

    finish commit

--- app.js
+++ app.js
@@ -1,5 +1,39 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
+const methodOverride = require('method-override');
+const Task = require('./models/tasks');
 
-mongoose.connect('mongodb://localhost:27017/ToDo');
+app.use(methodOverride('_method', {methods: ['POST', 'GET']}));
+app.set("view engine", "ejs");
+
+app.use(express.urlencoded({extended: true}));
+
+mongoose.connect('mongodb://localhost:27017/ToDo', {useNewUrlParser: true, useUnifiedTopology: true});
+
+
+// insert 1
+// app.get('/create', (req, res)=>{
+//     const firstTask = new Task({title: 'writing insert function'});
+//     firstTask.save().then(()=>console.log('new record inserted'));
+// });
+// insert 2
+// app.post('/create', );
+
+// find/show
+// app.get('/', );
+// delete
+// app.delete('/delete/:id', );
+// update
+// app.get('/update/:id/:title', (req, res)=>{
+//     Task.updateOne({_id: req.params.id}, {title: req.params.title},
+//         (error)=>{
+//         if(error) console.log(`there was an error: ${error}`);
+//         else console.log('Task is updated!');
+//     });
+// });
+// app.get('/update/:id/', );
+
+// app.put('/update/:id', );
+
+app.listen(3000, ()=>console.log('express started!'));
---
+++ controllers/tasks.js
@@ -0,0 +1,39 @@
+const Task = require("../models/tasks");
+
+module.exports = {
+    index: (req, res)=>{
+        Task.find({}, (error, tasks)=>{
+            if(error) console.log(`there was an error: ${error}`);
+            else {
+                // tasks.forEach(task => console.log(task));
+                res.render("todo.ejs", {todotasks: tasks});
+            }
+        });
+    },
+    create: (req, res)=>{
+        const firstTask = new Task({title: req.body.title});
+        firstTask.save().then(()=>res.redirect("/"));
+    },
+    edit: (req, res)=>{
+        const id = req.params.id;
+        Task.find({}, (err, tasks)=>{
+            res.render("todoEdit.ejs", {todotasks: tasks, idTask: id});
+        });
+    },
+    update: (req, res)=>{
+        const id = req.params.id;
+        Task.findByIdAndUpdate(id, {title: req.body.title}, err=>{
+            if(err) return res.send(500, err);
+            else res.redirect("/");
+        });
+    },
+    delete: (req, res)=>{
+        Task.deleteOne({_id: req.params.id}, (error)=>{
+            if(error) console.log(`there was an error: ${error}`);
+            else {
+                // tasks.forEach(task => console.log('one Task is deleted!'));
+                res.redirect("/");
+            }
+        });
+    }
+}
