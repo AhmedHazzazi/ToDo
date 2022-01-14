434b3769ad43f0d5118ea278ec689e553c943208
AhmedHazzazi <40734018+AhmedHazzazi@users.noreply.github.com>
2022-01-14 14:55:55 +0300

    My first commit

--- app.js
+++ app.js
@@ -9,8 +9,27 @@ app.set("view engine", "ejs");
 
 app.use(express.urlencoded({extended: true}));
 
-mongoose.connect('mongodb://localhost:27017/ToDo', {useNewUrlParser: true, useUnifiedTopology: true});
+// Database Connect
+if (process.env.NODE_ENV === "test")
+    mongoose.connect('mongodb://localhost:27017/Students_DB', 
+        { useNewUrlParser: true, useUnifiedTopology: false }
+    );
+else
+    mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/Students_DB', 
+        { useNewUrlParser: true, useUnifiedTopology: false }
+    );
+
+mongoose.set("useCreateIndex", true);
+const db = mongoose.connection;
+db.once("open", () => {
+    console.log("Successfully Connected To MongoDB Using Mongoose!");
+});
+// mongoose.connect('mongodb://localhost:27017/ToDo', {useNewUrlParser: true, useUnifiedTopology: true});
 
 app.use('/', router);
 
+// app.listen(3000, ()=>console.log('express started!'));
+// Port
 app.listen(3000, ()=>console.log('express started!'));
+if(process.env.NODE_ENV === "test") app.set("port", 3001);
+else app.set("port", process.env.PORT || 3000);
